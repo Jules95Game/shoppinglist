32ff3880711dd370b752a059324a60c1abd4e40a
Jules95Game <romp.julian1@gmail.com>
2024-05-30 12:49:44 +0200

    Frontend works

--- backend/src/main/java/org/jules/shopping_list/ShoppingListController.java
+++ backend/src/main/java/org/jules/shopping_list/ShoppingListController.java
@@ -2,10 +2,12 @@ package org.jules.shopping_list;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:5173")
 @RestController
 @RequestMapping("api/v1/shopping-list-items")
 public class ShoppingListController {
