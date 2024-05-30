d22e4eaebd140d5c4950220ed6ac61a24d7b8930
Jules95Game <romp.julian1@gmail.com>
2024-05-30 12:03:13 +0200

    Backend works

---
+++ backend/src/main/java/org/jules/shopping_list/ShoppingListController.java
@@ -0,0 +1,20 @@
+package org.jules.shopping_list;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/shopping-list-items")
+public class ShoppingListController {
+    @GetMapping
+    public List<String> get() {
+        return List.of(
+                "boter",
+                "kaas",
+                "eieren"
+        );
+    }
+}
